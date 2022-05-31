9a2cbdc6f3c20b3b662bb5fc40ec1c5c3b30beb2
hanklin <55573473+hank0316@users.noreply.github.com>
2022-05-31 12:01:20 +0800

    Change model

--- app/handlers/filter.py
+++ app/handlers/filter.py
@@ -6,17 +6,24 @@ from argparse import ArgumentParser, Namespace
 from pathlib import Path
 import numpy as np 
 
+def softmax(x):
+    f_x = np.exp(x) / np.sum(np.exp(x))
+    return f_x
+
 def classifier(mail):    
-    tokenizer = AutoTokenizer.from_pretrained("hfl/chinese-macbert-large")
+    intent2idx = {'ham': 0, 'spam': 1}
+    idx2intent = {idx: intent for intent, idx in intent2idx.items()}
+
+    tokenizer = AutoTokenizer.from_pretrained("ckiplab/albert-tiny-chinese")
     text = mail['subject'] + mail.body()
     print(text)
     tokens = tokenizer(text, padding='max_length', max_length=128, truncation=True, return_tensors='pt')
     print(tokens)
     # Preparing model
-    model = AutoModelForSequenceClassification.from_pretrained("/home/team7/test_project/best.ckpt", num_labels=2)
-    logits = model(**tokens)
-    prediction = np.argmax(logits, axis=-1)
-    print(prediction)
+    model = AutoModelForSequenceClassification.from_pretrained("/home/team7/test_project/best_smooth.ckpt", num_labels=2)
+    logits = softmax(model(**tokens)['logits'].detach().numpy())[0]
+    prediction = 1 if logits[1] >= 0.85 else 0
+    print(f"prediction: {idx2intent[prediction]}")
     return prediction
 
 
